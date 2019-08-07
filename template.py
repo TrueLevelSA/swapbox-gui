c10c3251da7bd5fd70d020b6b9ebea94383b5273
Nicolas Huguenin <nichuguen@gmail.com>
2019-08-07 16:23:10 +0100

    added ugly popup for languages to see how it works

--- template.py
+++ template.py
@@ -1,6 +1,10 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.popup import Popup
+from kivy.uix.modalview import ModalView
+from kivy.uix.label import Label
+from kivy.uix.button import Button
 from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition, RiseInTransition, FallOutTransition
 from kivy.properties import ObjectProperty, StringProperty, NumericProperty
@@ -16,6 +20,38 @@ Window.size = (1280, 720)
 #Window.borderless = True
 Window.fullscreen = True
 Window.show_cursor = False
+
+class ButtonLanguage(Button):
+    _language = StringProperty(None)
+    
+    def __init__(self, language, callback=None, **kwargs):
+        self._language = language
+        self._callback =  callback
+        #self.text = language
+        self.background_down = 'img/flags/large/' + language + '.png'
+        self.background_normal = self.background_down
+        super().__init__(**kwargs)
+
+    def on_press(self):
+        App.get_running_app().change_language(self._language)
+        if self._callback is not None:
+            self._callback()
+
+class LayoutPopup(BoxLayout):
+    pass
+
+class FullScreenPopup(ModalView):
+    pass
+
+class LanguagePopup(FullScreenPopup):
+    def __init__(self):
+        super().__init__()
+        # has to match the array in lang_template.yaml
+        languages = ['FR', 'EN']
+        wid = LayoutPopup()
+        for l in languages:
+            wid.add_widget(ButtonLanguage(l, self.dismiss))
+        self.add_widget(wid)
 
 class ScreenWelcome(Screen):
     pass
@@ -169,6 +205,7 @@ class TemplateApp(App):
     # try to share a variable in another way
     _chf_to_eth = NumericProperty(0)
     _eth_to_chf = NumericProperty(0)
+    _selected_language = StringProperty('English')
 
     def __init__(self, config):
         super().__init__()
