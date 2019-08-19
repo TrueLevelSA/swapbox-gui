6108cce0b9cf8ff18fafdcee66ff017eefaf2947
Nicolas Huguenin <nichuguen@gmail.com>
2019-08-19 21:52:52 +0100

    fixed some install and config stuff

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # also, opencv was installed using apt
 sudo apt install python3-opencv --fix-missing
 # to launch: (might need reboot?)
-DISPLAY=:0 KIVY_GL_BACKEND=sdl2 python3 template.py develop
+DISPLAY=:0 KIVY_GL_BACKEND=sdl2 KIVY_WINDOW=sdl2 python3 template.py develop
 # comment @lxpanel line in /etc/xdg/lxsession/LXDE-pi/autostart
 # add gpu_mem=512 in /boot/config.txt
 # add ",param=invert_y=0" to the "%(name)s = probesysfs,provider=hidinput" line under [input]
