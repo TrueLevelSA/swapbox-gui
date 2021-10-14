2de83dcc28ffc2f4bbf64645ad221133d71454ad
megaturbo <megaturbo@truelevel.ch>
2021-10-14 15:43:22 +0200

    chores: add copyright header

--- src_backends/config_tools.py
+++ src_backends/config_tools.py
@@ -13,6 +13,7 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import argparse
 from enum import auto, Enum
 import strictyaml
