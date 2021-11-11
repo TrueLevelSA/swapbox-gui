fa45439b2c2326600553e95308eb97c0c9a94d3f
megaturbo <megaturbo@truelevel.ch>
2021-11-11 11:00:30 +0100

    feat: update pricefeed mock with multi-currency response
    - format file

--- mock_services/mock_pricefeed.py
+++ mock_services/mock_pricefeed.py
@@ -26,19 +26,18 @@ context = zmq.Context()
 socket = context.socket(zmq.PUB)
 socket.bind('tcp://*:{}'.format(port))
 
-
 value = 100e18
 index = 0.1
-increment = 0.1/4
-
+increment = 0.1 / 4
 
 f = argument.Arguments()
-#add a switch, a flag with no argument
+# add a switch, a flag with no argument
 f.switch("verbose",
-    help="Verbose output",
-    abbr="v"
-)
+         help="Verbose output",
+         abbr="v"
+         )
 arguments, errors = f.parse()
+
 
 def noise_this_number(number, amplitude):
     global index
@@ -46,26 +45,39 @@ def noise_this_number(number, amplitude):
     index += increment
     return number + amplitude * math.sin(index)
 
+
 def noise_this_number_hex(number, amplitude):
     return hex(round(noise_this_number(number, amplitude)))[2:]
 
 
-obeuhjeuh = {
-        'eth_reserve': 20,
-        'token_reserve': 100,
-        'buy_fee': 120,
-        'sell_fee': 120
+pricefeed = {
+    'backend': 'zksync',
+    'base_currency': 'CHF',
+    'prices': {
+        'ETH': {
+            'price': 4000.0,
+            'buy_fee': 120,
+            'sell_fee': 120,
+        },
+        'DAI': {
+            'price': 0.9,
+            'buy_fee': 110,
+            'sell_fee': 110,
+        }
+    }
+
 }
 
 try:  # Command Interpreter
     while True:
-        obeuhjeuh['eth_reserve'] = noise_this_number_hex(1e18, 0.1e18)
-        obeuhjeuh['token_reserve'] = noise_this_number_hex(200e18, -10e18)
-        obeuhjeuh['buy_fee'] = 120
-        obeuhjeuh['sell_fee'] = 120
+        for token, price in pricefeed['prices'].items():
+            p = price['price']
+            pricefeed['prices'][token]['price'] = noise_this_number(p, p/1000)
+
         if arguments["verbose"]:
-            print(" Sending {}".format(obeuhjeuh))
-        socket.send_multipart(["priceticker".encode('utf-8'), json.dumps(obeuhjeuh).encode('utf-8')])
+            print(" Sending {}".format(pricefeed))
+
+        socket.send_multipart(["priceticker".encode('utf-8'), json.dumps(pricefeed).encode('utf-8')])
         sleep(1)
 
 except KeyboardInterrupt:  # If user do CTRL+C
